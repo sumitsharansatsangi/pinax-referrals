0965aa76ef9ef08064805aeacb343b13ac823dbb
Sumit Kumar <sharansumitkumar@gmail.com>
2021-12-20 02:07:04 +0000

    Update apps.py
    Added python 3.10 support

--- pinax/referrals/apps.py
+++ pinax/referrals/apps.py
@@ -1,6 +1,9 @@
+import platform
 from django.apps import AppConfig as BaseAppConfig
-from django.utils.translation import ugettext_lazy as _
-
+if platform.python_version() == "3.10.0":
+    from django.utils.translation import gettext_lazy as _
+else:
+    from django.utils.translation import ugettext_lazy as _
 
 class AppConfig(BaseAppConfig):
 
